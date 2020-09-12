d4a0e0df37783bd5ab3e892ecee3a818faf2ae54
Joseph Perry <josephperry720@gmail.com>
2020-09-12 14:24:16 -0400

    weather api call

--- assets/index.js
+++ assets/index.js
@@ -1,10 +1,24 @@
 var cityList = $(".list-group");
 
+function displayCurrentWeather(city) {
+    currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=f2f448fdff7880f3d298bdf08e187544"
+
+    request = {
+        url: currentWeatherURL,
+        method: "GET",
+    }
+
+    $.ajax(request).then(function(response) {
+        console.log(response);
+    })
+}
+
 $("#search-button").on("click", function(event) {
     event.preventDefault();
-    var cityInput = $("#search-input").val();
-    console.log(cityInput);
 
+    var cityInput = $("#search-input").val();
     var listEl = $("<li>").addClass("list-group-item").text(cityInput);
     cityList.append(listEl);
 })
+
+displayCurrentWeather("Atlanta")
