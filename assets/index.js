5cf7f61ee6247cec49a06c4f5c54042ab683e92b
Joseph Perry <josephperry720@gmail.com>
2020-09-12 13:56:56 -0400

    search button logs text as list element

--- assets/index.js
+++ assets/index.js
@@ -4,4 +4,7 @@ $("#search-button").on("click", function(event) {
     event.preventDefault();
     var cityInput = $("#search-input").val();
     console.log(cityInput);
+
+    var listEl = $("<li>").addClass("list-group-item").text(cityInput);
+    cityList.append(listEl);
 })
