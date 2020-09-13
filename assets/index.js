085f7cd3cfc719efa58f1da76687e98a8e7a5ed1
Joseph Perry <josephperry720@gmail.com>
2020-09-13 12:19:25 -0400

    city list populated on page load

--- assets/index.js
+++ assets/index.js
@@ -4,132 +4,165 @@ var cityList = $(".list-group");
 var currentDate = moment().format("M/D/YY");
 
 function storeWeather(city) {
+  currentWeatherURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&units=imperial&appid=f2f448fdff7880f3d298bdf08e187544";
 
-    currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=f2f448fdff7880f3d298bdf08e187544"
+  requestCurrent = {
+    url: currentWeatherURL,
+    method: "GET",
+  };
 
-    requestCurrent = {
-        url: currentWeatherURL,
-        method: "GET",
-    }
+  $.ajax(requestCurrent).then(function (responseOne) {
+    var lat = responseOne.coord.lat;
+    var long = responseOne.coord.lon;
 
-    $.ajax(requestCurrent).then(function(responseOne) {
+    uviURL =
+      "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+      lat +
+      "&lon=" +
+      long +
+      "&%20&exclude=daily,hourly,minutely&appid=f2f448fdff7880f3d298bdf08e187544";
 
-        var lat = responseOne.coord.lat;
-        var long = responseOne.coord.lon;
+    requestUVI = {
+      url: uviURL,
+      method: "GET",
+    };
 
-        uviURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&%20&exclude=daily,hourly,minutely&appid=f2f448fdff7880f3d298bdf08e187544"
-
-        requestUVI = {
-            url: uviURL,
-            method: "GET",
-        }
-
-        $.ajax(requestUVI).then(function(responseTwo) {
-            var currentStats = {
-                name: responseOne.name,
-                weather: responseOne.weather[0].main,
-                temperature: responseOne.main.temp + " °F",
-                humidity: responseOne.main.humidity + "%",
-                windSpeed: responseOne.wind.speed + " MPH",
-                uvIndex: responseTwo.current.uvi,
-                date: moment().utc(responseOne.dt).format("M/D/YY"), 
-            }
-            addToStorage("currentValues", currentStats);
-            displayCurrentWeather(city);
-        });
+    $.ajax(requestUVI).then(function (responseTwo) {
+      var currentStats = {
+        name: responseOne.name,
+        weather: responseOne.weather[0].main,
+        temperature: responseOne.main.temp + " °F",
+        humidity: responseOne.main.humidity + "%",
+        windSpeed: responseOne.wind.speed + " MPH",
+        uvIndex: responseTwo.current.uvi,
+        date: moment().utc(responseOne.dt).format("M/D/YY"),
+      };
+      addToStorage("currentValues", currentStats);
+      displayCurrentWeather(city);
     });
+  });
 
-    futureWeatherURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=f2f448fdff7880f3d298bdf08e187544"
+  futureWeatherURL =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    city +
+    "&units=imperial&appid=f2f448fdff7880f3d298bdf08e187544";
 
-    requestFuture = {
-        url: futureWeatherURL,
-        method: "GET",
+  requestFuture = {
+    url: futureWeatherURL,
+    method: "GET",
+  };
+
+  $.ajax(requestFuture).then(function (response) {
+    var futureStats = [];
+    // console.log(response);
+
+    start = moment(response.list[0].dt_txt);
+    start = start.add(12, "hours");
+    for (var i = 0; i < response.list.length; i++) {
+      if (
+        start.format("M/D/YY") ===
+        moment(response.list[i].dt_txt).format("M/D/YY")
+      ) {
+        futureObject = {
+          date: moment(response.list[i + 4].dt_txt).format("M/D/YY"),
+          name: response.city.name,
+          temperature: response.list[i + 4].main.temp,
+          humidity: response.list[i + 4].main.humidity,
+          weather: response.list[i + 4].weather[0].main,
+        };
+
+        start = start.add(1, "days");
+        futureStats.push(futureObject);
+      }
     }
-
-    $.ajax(requestFuture).then(function(response) {
-
-        var futureStats = [];
-        // console.log(response);
-
-        start = moment(response.list[0].dt_txt);
-        start = start.add(12, "hours");
-        for (var i = 0; i < response.list.length; i++) {
-            if (start.format("M/D/YY") === moment(response.list[i].dt_txt).format("M/D/YY")) {
-                futureObject = {
-                    date: moment(response.list[i + 4].dt_txt).format("M/D/YY"),
-                    name: response.city.name,
-                    temperature: response.list[i + 4].main.temp,
-                    humidity: response.list[i + 4].main.humidity,
-                    weather: response.list[i + 4].weather[0].main,
-                };
-
-                start = start.add(1, "days");
-                futureStats.push(futureObject);
-            }
-        }
-        addToStorage("futureValues", futureStats);
-    });
+    addToStorage("futureValues", futureStats);
+  });
 }
 
 function addToStorage(key, value) {
-    var storedLocal = JSON.parse(localStorage.getItem(key));
-    if (storedLocal) {
-        storedLocal.push(value);
-        localStorage.setItem(key, JSON.stringify(storedLocal));
-    } else {
-        var storedLocal = [];
-        storedLocal.push(value);
-        localStorage.setItem(key, JSON.stringify(storedLocal));
-    }
+  var storedLocal = JSON.parse(localStorage.getItem(key));
+  if (storedLocal) {
+    storedLocal.push(value);
+    localStorage.setItem(key, JSON.stringify(storedLocal));
+  } else {
+    var storedLocal = [];
+    storedLocal.push(value);
+    localStorage.setItem(key, JSON.stringify(storedLocal));
+  }
 }
 
 function displayCurrentWeather(city) {
+  var storedLocal = JSON.parse(localStorage.getItem("currentValues"));
+  for (var i = 0; i < storedLocal.length; i++) {
+    if (storedLocal[i].name.toLowerCase() === city.toLowerCase()) {
+      console.log("yep");
+      $("#city-date").text(
+        storedLocal[i].name + " (" + storedLocal[i].date + ") "
+      );
+      $("#temp").text("Temperature: " + storedLocal[i].temperature);
+      $("#humid").text("Humidity: " + storedLocal[i].humidity);
+      $("#wind").text("Wind Speed: " + storedLocal[i].windSpeed);
+
+      var currentIMG = $("<img>");
+      if (storedLocal[i].weather === "Clear") {
+        currentIMG.attr("src", "assets/images/sun.png");
+        currentIMG.attr("alt", "Sunny");
+      } else if (storedLocal[i].weather === "Clouds") {
+        currentIMG.attr("src", "assets/images/cloud.png");
+        currentIMG.attr("alt", "Cloudy");
+      } else if (storedLocal[i].weather === "Rain") {
+        currentIMG.attr("src", "assets/images/water.png");
+        currentIMG.attr("alt", "Rainy");
+      } else if (
+        storedLocal[i].weather === "Smoke" ||
+        storedLocal[i].weather === "Haze" ||
+        storedLocal[i].weather === "Fog"
+      ) {
+        currentIMG.attr("src", "assets/images/fog.png");
+        currentIMG.attr("alt", "Foggy");
+      }
+
+      $("#city-date").append(currentIMG);
+
+      var uvSpan = $("<span>").text(storedLocal[i].uvIndex);
+      if (storedLocal[i].uvIndex > 7) {
+        uvSpan.addClass("uv-severe");
+      } else if (storedLocal[i].uvIndex < 3) {
+        uvSpan.addClass("uv-favorable");
+      } else {
+        uvSpan.addClass("uv-moderate");
+      }
+
+      $("#uvIndex").empty().text("UV Index: ").append(uvSpan);
+    }
+  }
+}
+
+function displayCityList() {
     var storedLocal = JSON.parse(localStorage.getItem("currentValues"));
-    for (var i = 0; i < storedLocal.length; i++) {
-        if (storedLocal[i].name.toLowerCase() === city.toLowerCase()) {
-            console.log("yep");
-            $("#city-date").text(storedLocal[i].name + " (" + storedLocal[i].date + ") ");
-            $("#temp").text("Temperature: " + storedLocal[i].temperature);
-            $("#humid").text("Humidity: " + storedLocal[i].humidity);
-            $("#wind").text("Wind Speed: " + storedLocal[i].windSpeed);
-
-            var currentIMG = $("<img>");
-            if (storedLocal[i].weather === "Clear") {
-                currentIMG.attr("src", "assets/images/sun.png");
-                currentIMG.attr("alt", "Sunny");
-            } else if (storedLocal[i].weather === "Clouds") {
-                currentIMG.attr("src", "assets/images/cloud.png");
-                currentIMG.attr("alt", "Cloudy");
-            } else if (storedLocal[i].weather === "Rain") {
-                currentIMG.attr("src", "assets/images/water.png");
-                currentIMG.attr("alt", "Rainy");
-            } else if (storedLocal[i].weather === "Smoke" || storedLocal[i].weather === "Haze" || storedLocal[i].weather === "Fog") {
-                currentIMG.attr("src", "assets/images/fog.png");
-                currentIMG.attr("alt", "Foggy");
-            }
-
-            $("#city-date").append(currentIMG);
-
-            var uvSpan = $("<span>").text(storedLocal[i].uvIndex)
-            if (storedLocal[i].uvIndex > 7) {
-                uvSpan.addClass("uv-severe");
-            } else if (storedLocal[i].uvIndex < 3) {
-                uvSpan.addClass("uv-favorable");
-            } else {
-                uvSpan.addClass("uv-moderate");
-            }
-
-            $("#uvIndex").empty().text("UV Index: ").append(uvSpan);
+    if (storedLocal) {
+        cityList.empty();
+        for (var i = 0; i < storedLocal.length; i++) {
+            var listEl = $("<li>").addClass("list-group-item").text(storedLocal[i].name);
+            cityList.append(listEl);
         }
     }
 }
 
-$("#search-button").on("click", function(event) {
-    event.preventDefault();
+$(document).ready(function() {
 
-    var cityInput = $("#search-input").val();
-    var listEl = $("<li>").addClass("list-group-item").text(cityInput);
-    cityList.append(listEl);
+    displayCityList();
 
-    storeWeather(cityInput);
+    $("#search-button").on("click", function (event) {
+        event.preventDefault();
+      
+        var cityInput = $("#search-input").val();
+        var listEl = $("<li>").addClass("list-group-item").text(cityInput);
+        cityList.append(listEl);
+      
+        storeWeather(cityInput);
+      });
 })
